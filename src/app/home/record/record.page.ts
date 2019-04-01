7c03569a180c50878c4a1d7e0c74efdc7f9ff8ba
Daniel <jmzwcn@gmail.com>
2019-04-01 22:31:36 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/record/record.page.ts
+++ src/app/home/record/record.page.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './record.page.html',
   styleUrls: ['./record.page.scss'],
 })
-export class RecordPage implements OnInit {
+export class RecordPage {
 
-  constructor() { }
+  records = [];
 
-  ngOnInit() {
+  constructor() {
+    this.records[0] = { "time": "04.01.2019", "title": "标题", "image": "http://i3.cqnews.net/xfsc/attachement/jpg/site82/20110720/b8ac6f24467b0f90c51d5e.jpg", "content": "abc test" }
+    this.records[1] = { "time": "04.01.2019", "title": "标题", "image": "http://img3.xiazaizhijia.com/walls/20160927/mid_dec5fdacc3059ca.jpg", "content": "abc test" }
+    this.records[2] = { "time": "04.01.2019", "title": "标题", "mp4": "http://1254070582.vod2.myqcloud.com/2607b0c9vodtransgzp1254070582/a1cc8c7f5285890781256678829/v.f40.mp4", "content": "abc test" }
+    this.records[3] = { "time": "04.01.2019", "title": "标题", "image": "https://goss3.vcg.com/creative/vcg/800/version23/VCG21400436589.jpg", "content": "abc test" }
   }
 
 }
