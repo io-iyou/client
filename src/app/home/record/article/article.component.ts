6f5891db2e1ebacd44d3d3ca81366a7d1fa80248
Daniel <jmzwcn@gmail.com>
2019-04-06 20:56:00 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/record/article/article.component.ts
+++ src/app/home/record/article/article.component.ts
@@ -14,18 +14,10 @@ export class ArticleComponent implements OnInit {
 
   constructor(private navParams: NavParams) {
     this.article = (<Article>this.navParams.get('article')).toObject();
-    //this.imageBase64 = this.uint8ArrayToBase64((<Article>this.navParams.get('article')).getImage_asU8());
+    //this.imageBase64 = (<Article>this.navParams.get('article')).getImage_asB64();
   }
 
   ngOnInit() { }
 
-  uint8ArrayToBase64(uint8Value: Uint8Array): string {
-    var binary = '';
-    //var bytes = new Uint8Array(buffer);
-    for (var len = uint8Value.byteLength, i = 0; i < len; i++) {
-      binary += String.fromCharCode(uint8Value[i]);
-    }
-    return "data:image/jpeg;base64," + window.btoa(binary);
-  }
 }
 
