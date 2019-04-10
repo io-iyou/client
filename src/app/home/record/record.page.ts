4758020684bc3deedf9db76e05e63380136be911
Daniel <jmzwcn@gmail.com>
2019-04-10 22:19:45 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/record/record.page.ts
+++ src/app/home/record/record.page.ts
@@ -43,11 +43,11 @@ export class RecordPage implements OnInit {
   }
 
   options: CameraOptions = {
-    quality: 40,
+    quality: 80,
     correctOrientation: true,
     //allowEdit: true,
-    targetWidth: 100,
-    targetHeight: 100,
+    targetWidth: 500,
+    targetHeight: 500,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
